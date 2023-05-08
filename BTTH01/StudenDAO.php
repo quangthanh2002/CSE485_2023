e04e9144008dcb8fc0591ad4fed499ee262bcbf7
dungbeo26 <dungbeo26072002@gmail.com>
2023-05-08 14:20:47 +0700

    Create StudenDAO.php

---
+++ BTTH01/StudenDAO.php
@@ -0,0 +1,26 @@
+<?php
+    class StudentDAO {
+        private $students = array();
+    
+        public function create(Student $student) {
+            // code to create a student
+        }
+    
+        public function read($id) {
+            // code to read a student
+        }
+    
+        public function update(Student $student) {
+            // code to update a student
+        }
+    
+        public function delete($id) {
+            // code to delete a student
+        }
+    
+        public function getAll() {
+            // code to get all students
+            return $this->students;
+        }
+    }
+?>
